7cd9aa89f03b0f1d661d617fcc76041bac15c55b
sruthijj <149562598+sruthijj@users.noreply.github.com>
2025-01-20 18:57:39 +0530

    Create investment.py

---
+++ investment.py
@@ -0,0 +1,5 @@
+initial=int(input('Enter your initial investment ='))
+final=int(input('Enter your final investment ='))
+profit=final-initial
+return_rate=(profit/initial)*100
+print('Return rate =',return_rate,'%')
